735da754c0c65e2c92e4e7a3f917ed20bc2bedfe
Maurice Nicholson <maurice@mauricenicholson.com>
2025-01-07 22:47:05 +0000

    test: fix broken tests

--- packages/example-machines/src/__tests__/loginMachine.test.ts
+++ packages/example-machines/src/__tests__/loginMachine.test.ts
@@ -17,35 +17,30 @@ afterAll(() => {
 });
 
 test("conditionals are evaluated in order", () => {
-  const machine = loginMachine.newInstance();
+  let login = loginMachine.newInstance().start();
 
-  machine.start();
-  machine.send({ type: "LOGIN", username: "hackerman", password: "anything" });
-  expect(machine.state).toEqual({ name: "banned" });
+  login.send({ type: "LOGIN", username: "hackerman", password: "anything" });
+  expect(login.state).toEqual({ name: "banned" });
 
-  machine.stop();
-  machine.start();
-  machine.send({ type: "LOGIN", username: "trustme", password: "password123" });
-  expect(machine.state).toEqual({ name: "authenticated", username: "trustme", rememberMe: false });
-  machine.send({ type: "LOGOUT" });
-  expect(machine.state).toEqual({ name: "unauthenticated" });
+  login = loginMachine.newInstance().start();
+  login.send({ type: "LOGIN", username: "trustme", password: "password123" });
+  expect(login.state).toEqual({ name: "authenticated", username: "trustme", rememberMe: false });
+  login.send({ type: "LOGOUT" });
+  expect(login.state).toEqual({ name: "unauthenticated" });
 
-  machine.stop();
-  machine.start();
-  machine.send({ type: "LOGIN", username: "trustme", password: "password123" });
-  expect(machine.state).toEqual({ name: "authenticated", username: "trustme", rememberMe: false });
+  login = loginMachine.newInstance().start();
+  login.send({ type: "LOGIN", username: "trustme", password: "password123" });
+  expect(login.state).toEqual({ name: "authenticated", username: "trustme", rememberMe: false });
   clock.tick(1000 * 60 * 5); // user is automatically logged out after 5 minutes
-  expect(machine.state).toEqual({ name: "unauthenticated" });
+  expect(login.state).toEqual({ name: "unauthenticated" });
 
-  machine.stop();
-  machine.start();
-  machine.send({ type: "LOGIN", username: "trustme", password: "wrong" });
-  expect(machine.state).toEqual({ name: "invalidCredentials", errorMessage: "Incorrect password" });
+  login = loginMachine.newInstance().start();
+  login.send({ type: "LOGIN", username: "trustme", password: "wrong" });
+  expect(login.state).toEqual({ name: "invalidCredentials", errorMessage: "Incorrect password" });
 
-  machine.stop();
-  machine.start();
-  machine.send({ type: "LOGIN", username: "someone", password: "whatever" });
-  expect(machine.state).toEqual({
+  login = loginMachine.newInstance().start();
+  login.send({ type: "LOGIN", username: "someone", password: "whatever" });
+  expect(login.state).toEqual({
     name: "invalidCredentials",
     errorMessage: 'Unknown username "someone" or incorrect password',
   });
