8549e91355c5bacb41e87436cc3f18421bf70044
Maurice Nicholson <maurice@mauricenicholson.com>
2025-01-11 21:03:33 +0000

    docs: automate bundlephobia screenshot scraping + docs updates

--- scripts/sync-docs.ts
+++ scripts/sync-docs.ts
@@ -1,4 +1,4 @@
-import { readdir, readFile, writeFile } from "node:fs/promises";
+import { readFile, readdir, writeFile } from "node:fs/promises";
 import { type Locator, chromium, devices } from "playwright";
 
 interface PackageMetadata {
